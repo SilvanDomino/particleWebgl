9e18c0756d7156e85291fb8a97378304b094b8df
dice9907 <22070@ma-web.nl>
2018-06-12 22:08:10 +0200

    moved to half to a module

--- src/background/canvas_module.js
+++ src/background/canvas_module.js
@@ -11,64 +11,13 @@ import simulationFrag1 from './shaders/simulationFrag01.glsl';
 import simulationFrag2 from './shaders/simulationFrag02.glsl';
 import simulationFrag3 from './shaders/simulationFrag03.glsl';
 
-import {Float16Array} from '@petamoriken/float16';
-
-var toHalf = (function() {
-
-  var floatView = new Float32Array(1);
-  var int32View = new Int32Array(floatView.buffer);
-
-  /* This method is faster than the OpenEXR implementation (very often
-   * used, eg. in Ogre), with the additional benefit of rounding, inspired
-   * by James Tursa?s half-precision code. */
-  return function toHalf(val) {
-
-    floatView[0] = val;
-    var x = int32View[0];
-
-    var bits = (x >> 16) & 0x8000; /* Get the sign */
-    var m = (x >> 12) & 0x07ff; /* Keep one extra bit for rounding */
-    var e = (x >> 23) & 0xff; /* Using int is faster here */
-
-    /* If zero, or denormal, or exponent underflows too much for a denormal
-     * half, return signed zero. */
-    if (e < 103) {
-      return bits;
-    }
-
-    /* If NaN, return NaN. If Inf or exponent overflow, return Inf. */
-    if (e > 142) {
-      bits |= 0x7c00;
-      /* If exponent was 0xff and one mantissa bit was set, it means NaN,
-                   * not Inf, so make sure we set one mantissa bit too. */
-      bits |= ((e == 255) ? 0 : 1) && (x & 0x007fffff);
-      return bits;
-    }
-
-    /* If exponent underflows but not too much, return a denormal */
-    if (e < 113) {
-      m |= 0x0800;
-      /* Extra rounding may overflow and set mantissa to 0 and exponent
-       * to 1, which is OK. */
-      bits |= (m >> (114 - e)) + ((m >> (113 - e)) & 1);
-      return bits;
-    }
-
-    bits |= ((e - 112) << 10) | (m >> 1);
-    /* Extra rounding. An overflow will set mantissa to 0 and increment
-     * the exponent, which is OK. */
-    bits += m & 1;
-    return bits;
-  };
-
-}());
+import toHalf from './toHalf';
 
 const particles = 16384;
 
 class Canvas {
   constructor() {
     this._canvas = document.getElementsByClassName('background-js')[0];
-    console.log(Float16Array);
     this._canvas.width = window.innerWidth;
     this._canvas.height = window.innerHeight;
 
